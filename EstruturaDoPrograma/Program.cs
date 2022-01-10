9235ef68d7a1dbe33169d556202a55134a11cf93
thiago <thiagonascimento2122@gmail.com>
2022-01-10 19:29:11 -0300

    primeiros passos com .net dio

---
+++ EstruturaDoPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstruturaDoPrograma
+{
+    class EstruturaDoPrograma
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilhar(1);
+            s.Empilhar(10);
+            s.Empilhar(100);
+            Console.WriteLine(s.Desempilha());
+             Console.WriteLine(s.Desempilha());
+              Console.WriteLine(s.Desempilha());
+
+        }
+    }
+}
